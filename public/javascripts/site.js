f46736e05a84defb9ec2a682a642b05e1c80ac0e
Jeff Dickey <dickeytk@gmail.com>
2010-12-18 00:47:34 -0800

    Fixed animation

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,20 +1,20 @@
 function setup_cards() {
   $('#top-card').click(function() {
-    $('#middle-card').animate({top: '+=170', left:'-=5'}, 'fast', function() {
+    $('#middle-card').animate({top: '+=320', left:'-=5'}, 'fast', function() {
       $('#middle-card').css('z-index', top_card_z_index++);
-      $('#middle-card').animate({top: '-=170', left:'+=6'}, 'fast');
+      $('#middle-card').animate({top: '-=320', left:'+=6'}, 'fast');
     });
   });
   $('#middle-card').click(function() {
-    $('#bottom-card').animate({top: '+=170', left:'+=12'}, 'fast', function() {
+    $('#bottom-card').animate({top: '+=320', left:'+=12'}, 'fast', function() {
       $('#bottom-card').css('z-index', top_card_z_index++);
-      $('#bottom-card').animate({top: '-=170', left:'-=11'}, 'fast');
+      $('#bottom-card').animate({top: '-=320', left:'-=11'}, 'fast');
     });
   });
   $('#bottom-card').click(function() {
-    $('#top-card').animate({top: '+=170', left:'+=1'}, 'fast', function() {
+    $('#top-card').animate({top: '+=320', left:'+=1'}, 'fast', function() {
       $('#top-card').css('z-index', top_card_z_index++);
-      $('#top-card').animate({top: '-=170'}, 'fast');
+      $('#top-card').animate({top: '-=320'}, 'fast');
     });
   });
 }
