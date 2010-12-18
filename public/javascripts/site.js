a5aa3b7d348a6b55b32c236caada29587498c716
Jeff Dickey <dickeytk@gmail.com>
2010-12-17 22:16:18 -0800

    Finished tour and most of the copy

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,22 +1,4 @@
 function setup_cards() {
-  $('#top-card').click(function() {
-    $('#middle-card').animate({top: '+=170', left:'-=5'}, 'fast', function() {
-      $('#middle-card').css('z-index', top_card_z_index++);
-      $('#middle-card').animate({top: '-=170', left:'+=6'}, 'fast');
-    });
-  });
-  $('#middle-card').click(function() {
-    $('#bottom-card').animate({top: '+=170', left:'+=12'}, 'fast', function() {
-      $('#bottom-card').css('z-index', top_card_z_index++);
-      $('#bottom-card').animate({top: '-=170', left:'-=11'}, 'fast');
-    });
-  });
-  $('#bottom-card').click(function() {
-    $('#top-card').animate({top: '+=170', left:'+=1'}, 'fast', function() {
-      $('#top-card').css('z-index', top_card_z_index++);
-      $('#top-card').animate({top: '-=170'}, 'fast');
-    });
-  });
 }
 
 function setup_login() {
@@ -26,7 +8,7 @@ function setup_login() {
       loginform.css('top', '');
     }
     else {
-      loginform.css('top', '0px');
+      loginform.css('top', '1px');
       $('#login-username').focus();
     }
   });
