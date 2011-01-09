9b478a5e20762d80f8a4bbfa786447ae63e6ca9e
Jeff Dickey <dickeytk@gmail.com>
2011-01-09 10:23:02 -0800

    Added cta helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def cta_link_to(text, url)
+    html = "<span class='cta-button'>
+              <span>
+                <a href='#{ url }'>#{ text }</a>
+              </span>
+            </span>"
+  end
 end
