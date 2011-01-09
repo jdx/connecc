6ac0e31e68be98eab7aeb7b31bd582fd8e4a3b9e
Jeff Dickey <dickeytk@gmail.com>
2011-01-09 11:51:54 -0800

    Documenting

--- app/controllers/google_checkout_api_controller.rb
+++ app/controllers/google_checkout_api_controller.rb
@@ -8,39 +8,49 @@ class GoogleCheckoutApiController < ApplicationController
 
   def callback
 
+    # Get the serial number we need to send back to google to get the actual notification
     serial_number = request.POST["serial-number"]
 
+    # Set up the server we're going to talk to
     url = URI.parse(GOOGLE_CHECKOUT_NOTIFICATION_HISTORY_URL)
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
+    # Set up the headers for the request (crazy base64 madness is per the api spec)
     header = {
       "Authorization" => "Basic " + Base64.encode64("#{ ENV['CONNECC_GOOGLE_MERCHANT_ID'] }:#{ ENV['CONNECC_GOOGLE_MERCHANT_KEY']}"),
       "Content-Type" => "application/xml;charset=UTF-8",
       "Accept" => "application/xml;charset=UTF-8"
     }
 
+    # create the request
     req = Net::HTTP::Post.new(url.path, header)
 
+    # xml we're going to send
     req.body = "
       <notification-history-request xmlns=\"http://checkout.google.com/schema/2\">
         <serial-number>#{ serial_number }</serial-number>
       </notification-history-request>
     "
 
+    # Make the request
     res = http.start { |http| http.request(req) }
 
+    # Error out if not a success
     unless res.kind_of? Net::HTTPSuccess
       res.error!
     end
 
+    # show the log of the xml returned
     puts res.body
 
+    # Set up our parser
     frontend = Google4R::Checkout::Frontend.new(FRONTEND_CONFIGURATION)
     frontend.tax_table_factory = TaxTableFactory.new
     handler = frontend.create_notification_handler
 
+    # parse the data
     begin
       notification = handler.handle(res.body)
     rescue Google4R::Checkout::UnknownNotificationType => e
@@ -48,6 +58,7 @@ class GoogleCheckoutApiController < ApplicationController
       return
     end
 
+    # Find out what type of notification it is and handle it accordingly
     if notification.kind_of? Google4R::Checkout::NewOrderNotification
       order = Order.create! do |o|
         o.buyer_billing_address = Address.create! do |a|
