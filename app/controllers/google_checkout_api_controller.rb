9e3af411c2bb80bd4fc6edb8e9395533a823313a
Jeff Dickey <dickeytk@gmail.com>
2011-01-08 21:03:51 -0800

    Fixed imports

--- app/controllers/google_checkout_api_controller.rb
+++ app/controllers/google_checkout_api_controller.rb
@@ -46,7 +46,7 @@ class GoogleCheckoutApiController < ApplicationController
       return
     end
 
-    if notification.kind_of? NewOrderNotification
+    if notification.kind_of? Google4R::Checkout::NewOrderNotification
       order = Order.create! do |o|
         o.buyer_billing_address = Address.create_from_google(notification.buyer_billing_address)
         o.buyer_id = notification.buyer_id
@@ -55,7 +55,7 @@ class GoogleCheckoutApiController < ApplicationController
         o.fulfillment_order_state = notification.fulfillment_order_state
         o.google_order_number = notification.google_order_number
       end
-    elsif notification.kind_of? OrderStateChangeNotification
+    elsif notification.kind_of? Google4R::Checkout::OrderStateChangeNotification
       order = Order.find_by_google_order_number(notification.google_order_number)
       order.financial_order_state = order.new_financial_order_state
       order.fulfillment_order_state = order.new_fulfillment_order_state
