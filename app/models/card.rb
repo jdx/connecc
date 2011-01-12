8f8043f3e289de3f45d0d129a87bb87551907879
Jeff Dickey <dickeytk@gmail.com>
2011-01-11 20:46:44 -0800

    Duplicate checking of codes

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,6 +7,8 @@ class Card < ActiveRecord::Base
   has_many :contact_requests
   has_many :notification_requests
 
+  validates_uniqueness_of :code
+
   attr_accessible :message
 
   def giver
@@ -23,6 +25,11 @@ class Card < ActiveRecord::Base
     chars = 'abcdefghjkmnpqrstuvwxyz23456789'
     self.code = ''
     5.times { |i| self.code << chars[rand(chars.length)] }
+
+    # check for duplicates
+    if Card.find_by_code(self.code)
+      self.generate_code
+    end
   end
 
 end
