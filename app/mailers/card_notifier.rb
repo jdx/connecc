21f9a2072a6cfb2ec777cd731de69f94fc790396
Jeff Dickey <dickeytk@gmail.com>
2011-01-11 21:33:27 -0800

    Checking for blank strings instead of null

--- app/mailers/card_notifier.rb
+++ app/mailers/card_notifier.rb
@@ -2,9 +2,9 @@ class CardNotifier < BaseNotifier
   def contact_request(contact_request)
     @contact_request = contact_request
     mail(:to => contact_request.card.giver.email_address_with_name,
-         :subject => @contact_request.message ?
-                     "conne.cc: \"#{ help.truncate @contact_request.message }\"" :
-                     "Someone you gave your conne.cc card to wants to get back in touch!")
+         :subject => @contact_request.message.blank? ?
+                     "Someone you gave your conne.cc card to wants to get back in touch!" :
+                     "conne.cc: \"#{ help.truncate @contact_request.message }\"")
   end
 
   def notification_request(notification_request)
