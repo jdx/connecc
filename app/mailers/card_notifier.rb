6070af253f1fa98a047b91da06e887c0ad4ea628
Jeff Dickey <dickeytk@gmail.com>
2011-01-11 21:01:37 -0800

    Updated emails

--- app/mailers/card_notifier.rb
+++ app/mailers/card_notifier.rb
@@ -2,12 +2,14 @@ class CardNotifier < BaseNotifier
   def contact_request(contact_request)
     @contact_request = contact_request
     mail(:to => contact_request.card.giver.email_address_with_name,
-         :subject => "Someone you gave your conne.cc card to wants to get back in touch!")
+         :subject => @contact_request.message ?
+                     "conne.cc: \"#{ @notification_request.card.truncate }\"" :
+                     "Someone you gave your conne.cc card to wants to get back in touch!")
   end
 
   def notification_request(notification_request)
     @notification_request = notification_request
     mail(:to => notification_request.email,
-         :subject => "#{ @notification_request.card.giver } has updated his conne.cc card for you")
+         :subject => "conne.cc: #{ @notification_request.card.giver } said \"#{ @notification_request.card.truncate }\"")
   end
 end
