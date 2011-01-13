e0af9808512cc7006842890c51fc28ed6965fe89
Jeff Dickey <dickeytk@gmail.com>
2011-01-13 13:37:04 -0800

    Fixing subject lines

--- app/mailers/card_notifier.rb
+++ app/mailers/card_notifier.rb
@@ -5,13 +5,13 @@ class CardNotifier < BaseNotifier
          :reply_to => @contact_request.email,
          :subject => @contact_request.message.blank? ?
                      "Someone you gave your conne.cc card to wants to get back in touch!" :
-                     "conne.cc: \"#{ help.truncate @contact_request.message.gsub(/\n/, ' ') }\"")
+                     "conne.cc: \"#{ help.truncate @contact_request.message.tr('\n', ' ') }\"")
   end
 
   def notification_request(notification_request)
     @notification_request = notification_request
     mail(:to => notification_request.email,
          :reply_to => @notification_request.card.giver.email_address_with_name,
-         :subject => "conne.cc: #{ @notification_request.card.giver } said \"#{ help.truncate @notification_request.card.message.gsub(/\n/, ' ') }\"")
+         :subject => "conne.cc: #{ @notification_request.card.giver } said \"#{ help.truncate @notification_request.card.message.tr('\n', ' ') }\"")
   end
 end
