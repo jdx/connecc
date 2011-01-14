43717d542165c18bfee62cfaa6a4d2df95c284a6
Jeff Dickey <dickeytk@gmail.com>
2011-01-13 21:03:21 -0800

    Tightened up contact info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  before_update :clean_contact_info
+
   has_many :orders
   has_many :visits
   has_many :contact_requests
@@ -12,11 +14,11 @@ class User < ActiveRecord::Base
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :gender, :presence => true
   validates :time_zone, :presence => true
-  validates_format_of :twitter, :with => /^@[a-z0-9_]*$/, :allow_blank => true, :allow_nil => true
+  validates_format_of :twitter, :with => /^@?[a-z0-9_]*$/, :allow_blank => true, :allow_nil => true
   validates_format_of :phone_number, :with => /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/, :allow_blank => true, :allow_nil => true
   validates_format_of :web_site, :with => /^([\w-]+:\/\/)?(www[.])?[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/))$/, :allow_blank => true, :allow_nil => true
-  validates_format_of :facebook, :with => /^([\w-]+:\/\/)?(www[.])?facebook\.com\/[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/))$/, :allow_blank => true, :allow_nil => true
-  validates_format_of :linkedin, :with => /^([\w-]+:\/\/)?(www[.])?linkedin\.com\/in\/[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/))$/, :allow_blank => true, :allow_nil => true
+  validates_format_of :facebook, :with => /^.*facebook\.com\/[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/))$/, :allow_blank => true, :allow_nil => true
+  validates_format_of :linkedin, :with => /^.*linkedin\.com\/in\/[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/))$/, :allow_blank => true, :allow_nil => true
 
   devise :database_authenticatable,
          :lockable,
@@ -60,11 +62,20 @@ class User < ActiveRecord::Base
   end
 
   def any_contact_info_public?
-    if show_email or facebook or linkedin or twitter or web_site or phone_number
+    if show_email or !facebook.blank? or !linkedin.blank? or !twitter.blank? or !web_site.blank? or !phone_number.blank?
       return true
     else
       return false
     end
+  end
+
+  protected
+
+  def clean_contact_info
+    self.web_site = "http://" + self.web_site if self.web_site and not self.web_site =~ /^https?:\/\//
+    self.facebook.gsub!(/.*facebook.com/, 'facebook.com') if self.facebook
+    self.linkedin.gsub!(/.*linkedin.com/, 'linkedin.com') if self.linkedin
+    self.twitter.gsub!(/^@?/, '@') if self.twitter
   end
 
 end
