d57dd2fda8a707352fe3387f2318b33de431e724
Wyatt Allen <wyatt@velociraptor.(none)>
2011-01-15 21:32:58 -0800

    Updated wrap-tabs. Removed menu login.

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,37 +1,78 @@
-function setup_cards() {
-  $('#top-card').click(function() {
-    $('#middle-card').animate({top: '+=320', left:'-=5'}, 'fast', function() {
-      $('#middle-card').css('z-index', top_card_z_index++);
-      $('#middle-card').animate({top: '-=320', left:'+=6'}, 'fast');
-    });
-  });
-  $('#middle-card').click(function() {
-    $('#bottom-card').animate({top: '+=320', left:'+=12'}, 'fast', function() {
-      $('#bottom-card').css('z-index', top_card_z_index++);
-      $('#bottom-card').animate({top: '-=320', left:'-=11'}, 'fast');
-    });
-  });
-  $('#bottom-card').click(function() {
-    $('#top-card').animate({top: '+=320', left:'+=1'}, 'fast', function() {
-      $('#top-card').css('z-index', top_card_z_index++);
-      $('#top-card').animate({top: '-=320'}, 'fast');
-    });
-  });
-}
+var Connecc = {
 
-function setup_login() {
-  $('#login-tab').click(function() {
-    var loginform = $('#site-login');
-    loginform.toggleClass('display');
-    // $('#login-username').focus();
-    return false;
-  });
-}
+    setupCards:function() {
+      $('#top-card')
+          .click(function() {
+              $('#middle-card')
+                  .animate(
+                      { top:'+=320', left:'-=5' }, 
+                      'fast', 
+                      function() {
+                          $('#middle-card')
+                              .css('z-index', Connecc.topCardZIndex++);
+                          $('#middle-card')
+                              .animate(
+                                  { top:'-=320', left:'+=6' }, 
+                                  'fast'
+                              );
+                      }
+                  );
+          });
+      
+      $('#middle-card')
+          .click(function() {
+              $('#bottom-card')
+                  .animate(
+                      { top:'+=320', left:'+=12' }, 
+                      'fast', 
+                      function() {
+                          $('#bottom-card')
+                              .css('z-index', Connecc.topCardZIndex++);
+                          $('#bottom-card')
+                              .animate(
+                                  { top:'-=320', left:'-=11' }, 
+                                  'fast'
+                              );
+                      }
+                  );
+          });
+          
+      $('#bottom-card')
+          .click(function() {
+              $('#top-card')
+                  .animate(
+                      { top:'+=320', left:'+=1' }, 
+                      'fast', 
+                      function() {
+                          $('#top-card')
+                              .css('z-index', Connecc.topCardZIndex++);
+                          $('#top-card')
+                              .animate(
+                                { top:'-=320' }, 
+                                'fast'
+                          );
+                      }
+                  );
+          });
+    },
 
-top_card_z_index = 3;
+    setupLogin:function() {
+      $('#login-tab, #loginWrapTab')
+          .click(function() {
+              $('#site-login')
+                  .toggleClass('display');
+              
+              // $('#login-username').focus();
+              
+              return false;
+          });
+    },
 
-$(document).ready(function() {
-  setup_cards();
-  setup_login();
+    topCardZIndex:3
+};
+
+$(function() {
+  Connecc.setupCards();
+  Connecc.setupLogin();
 });
 
