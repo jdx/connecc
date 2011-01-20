728ddf875eda42b91893a54a5c93a655ebde8e8e
Jeff Dickey <dickeytk@gmail.com>
2011-01-19 19:42:38 -0800

    Added company name

--- db/migrate/20101222212648_devise_create_users.rb
+++ db/migrate/20101222212648_devise_create_users.rb
@@ -9,6 +9,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.boolean :admin, :null => false, :default => false
       t.string :first_name, :null => false
       t.string :last_name, :null => false
+      t.string :company_name
       t.string :time_zone, :null => false, :default =>  "Pacific Time (US & Canada)"
       t.string :gender, :null => false, :limit => 1, :default => 'm'
       t.boolean :show_email, :null => false, :default => true
