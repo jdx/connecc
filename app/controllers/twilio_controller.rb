9eac69df0348d8935661c8d533b98945c870ccff
Jeff Dickey <dickeytk@gmail.com>
2011-01-21 09:31:32 -0800

    Apparently twilio is case sensitive

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -7,18 +7,18 @@ class TwilioController < ApplicationController
 
   def call
     render :text => '<?xml version="1.0" encoding="UTF-8"?>
-                     <response>
-                       <say>Thank you for calling kuhnec.</say>
-                       <gather numDigits="1" method="POST" action="handle_digits">
+                     <Response>
+                       <Say>Thank you for calling kuhnec.</Say>
+                       <Gather numDigits="1" method="POST" action="handle_digits">
                          Press 1 for support. Press 2 for sales.
-                       </gather>
-                     </response>', :content_type => 'text/xml'
+                       </Gather>
+                     </Response>', :content_type => 'text/xml'
   end
 
   def handle_digits
     render :text => '<?xml version="1.0" encoding="UTF-8"?>
-                     <response>
-                       <dial>971-222-7154</say>
-                     </response>', :content_type => 'text/xml'
+                     <Response>
+                       <Dial>971-222-7154</Dial>
+                     </Response>', :content_type => 'text/xml'
   end
 end
