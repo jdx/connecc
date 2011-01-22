688d9b21dfc4221e5bc0cf41758cee76433bba6f
Jeff Dickey <dickeytk@gmail.com>
2011-01-21 20:46:06 -0800

    Fixed up 404 tracking

--- app/controllers/cards/cards_controller.rb
+++ app/controllers/cards/cards_controller.rb
@@ -5,8 +5,7 @@ class Cards::CardsController < ApplicationController
   before_filter :ensure_user_is_giver, :only => [:edit, :update]
 
   def show
-    card_404s = Rails.cache.read(card_404_cache_key(request)) || []
-    puts card_404s
+    card_404s = Rails.cache.read(card_404_accesses_cache_key(request)) || []
     if card_404s.count > 10
       raise ActiveRecord::RecordNotFound
     end
@@ -40,16 +39,19 @@ class Cards::CardsController < ApplicationController
 
   protected
 
-  def card_404_cache_key(request)
-    cache_key = "ip:#{ request.remote_ip }_card404s"
+  def card_404_accesses_cache_key(request)
+    cache_key = "ip:#{ request.remote_ip }_card_404_accesses"
   end
 
   def get_card
     @card = Card.find_by_code(params[:code])
     unless @card
-      cards = Rails.cache.read(card_404_cache_key(request)) || []
+      # security to prevent scraping
+      cards = Rails.cache.read(card_404_accesses_cache_key(request)) || []
+      cards = cards.dup
       cards << params[:code] unless cards.include? params[:code]
-      Rails.cache.write card_404_cache_key(request), cards, :expires_in => 10.minutes
+      Rails.cache.write card_404_accesses_cache_key(request), cards, :expires_in => 10.minutes
+
       raise ActiveRecord::RecordNotFound
     end
   end
