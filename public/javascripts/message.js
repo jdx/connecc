d5b9a0c9a6ae9cd640b694e5dd9a18ccb59748d5
Jeff Dickey <dickeytk@gmail.com>
2011-01-23 20:22:32 -0800

    Fixed message for IE7

--- public/javascripts/message.js
+++ public/javascripts/message.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#message .open-form').click(function() {
+    $('.no-ie #message .open-form').click(function() {
         $(this).addClass('no-transitions');
         $(this).fadeOut(500, function() {
           $('.hidden-form').slideDown();
