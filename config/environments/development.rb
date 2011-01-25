43edd0e7e84d2ca33b970872b7a140db09cc3f77
Jeff Dickey <dickeytk@gmail.com>
2011-01-24 18:48:55 -0800

    adding code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,6 @@ Connecc::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.gem "rmagick", :lib => "RMagick"
 end
 
