79eba04d8bb1ce88387cd5a224bfdb788aff5b7a
Jeff Dickey <dickeytk@gmail.com>
2011-01-24 16:41:49 -0800

    Fixed admin controller

--- app/controllers/admin/delayed_jobs_controller.rb
+++ app/controllers/admin/delayed_jobs_controller.rb
@@ -1,11 +1,11 @@
 class Admin::DelayedJobsController < Admin::AdminController
 
   def index
-    @delayed_jobs = DelayedJob.order(:id)
+    @delayed_jobs = Delayed::Job.order(:id)
   end
 
   def show
-    @delayed_job = DelayedJob.find(params[:id])
+    @delayed_job = Delayed::Job.find(params[:id])
   end
 
 end
