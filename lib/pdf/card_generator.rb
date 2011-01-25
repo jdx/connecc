48a2fe75673a8b2d4c1f9419d06e383368f223bd
Jeff Dickey <dickeytk@gmail.com>
2011-01-25 13:09:52 -0800

    Cleaning up formatting when names change

--- lib/pdf/card_generator.rb
+++ lib/pdf/card_generator.rb
@@ -88,14 +88,46 @@ module PDF
     protected
 
     def self.render_card(pdf, data, card)
-      pdf.image Graphics::Gradient.new(0.9.in, "ff0000", "0000ff"), :at => [ 0.in, 1.in ], :width => 0.9.in, :height => 1.in
+      padding = 0.2.in
+      gradient_width = padding + pdf.width_of(data[:first_name], :size => 15) + 0.04.in
+      pdf.image Graphics::Gradient.new(gradient_width, "000055", "0000ff"), :at => [ 0.in, 1.in ], :width => gradient_width, :height => 1.in
+      self.render_name(pdf, data[:first_name], data[:last_name], data[:company_name], padding, gradient_width)
+      self.render_logo(pdf, "#{ Rails.root.to_s }/lib/assets/logo.2.png")
+      self.render_url(pdf, "http://conne.cc/#{ card.code }", gradient_width)
+    end
+
+    def self.render_name(pdf, first_name, last_name, company_name, padding, gradient_width)
       pdf.fill_color = "ffffff"
-      pdf.draw_text data[:first_name], :at => [0.32.in, 0.7.in], :size => 15
-      pdf.fill_color = "000000"
-      pdf.draw_text data[:last_name], :at => [0.9.in, 0.7.in], :size => 15
-      pdf.draw_text data[:company_name], :at => [0.7.in, 0.5.in], :size => 12
-      pdf.image "#{ Rails.root.to_s }/lib/assets/logo.2.png", :at => [2.65.in, 0.95.in], :width => 0.75.in
-      pdf.draw_text "http://conne.cc/#{ card.code }", :at => [1.in, 0.1.in], :size => 16
+      total_name_width = gradient_width + pdf.width_of(last_name)
+      if total_name_width > 2.in
+        pdf.draw_text first_name, :at => [padding, 0.7.in], :size => 15
+        pdf.fill_color = "000000"
+        pdf.draw_text last_name, :at => [gradient_width + 0.02.in, 0.7.in], :size => 12
+        pdf.draw_text company_name, :at => [gradient_width + 0.02.in, 0.5.in], :size => 10
+      else
+        pdf.draw_text first_name, :at => [padding, 0.7.in], :size => 15
+        pdf.fill_color = "000000"
+        pdf.draw_text last_name, :at => [gradient_width + 0.02.in, 0.7.in], :size => 15
+        pdf.draw_text company_name, :at => [gradient_width + 0.02.in, 0.5.in], :size => 12
+      end
+    end
+
+    def self.render_url(pdf, url, gradient_width)
+      if gradient_width > 1.7.in
+        pdf.draw_text url, :at => [1.85.in, 0.1.in], :size => 12
+      elsif gradient_width > 1.5.in
+        pdf.draw_text url, :at => [1.65.in, 0.1.in], :size => 13
+      elsif gradient_width > 1.3.in
+        pdf.draw_text url, :at => [1.5.in, 0.1.in], :size => 14
+      elsif gradient_width > 1.1.in
+        pdf.draw_text url, :at => [1.3.in, 0.1.in], :size => 15
+      else
+        pdf.draw_text url, :at => [1.2.in, 0.1.in], :size => 16
+      end
+    end
+
+    def self.render_logo(pdf, logo)
+      pdf.image logo, :at => [2.65.in, 0.95.in], :width => 0.75.in
     end
   end
 end
