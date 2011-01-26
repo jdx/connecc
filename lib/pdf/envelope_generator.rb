b6b6c4d47820e5ded8c218047301d6f182bf49c7
Jeff Dickey <dickeytk@gmail.com>
2011-01-25 20:52:42 -0800

    Using cabin for envelopes

--- lib/pdf/envelope_generator.rb
+++ lib/pdf/envelope_generator.rb
@@ -13,7 +13,7 @@ module PDF
       pdf.bounding_box [1.2.in, 3.55.in], :width => 3.5.in, :height => 2.in do
         pdf.fill_color "1D4088"
         pdf.text 'conne.cc'
-        pdf.font "Helvetica", :size => 12
+        pdf.font "Cabin", :size => 12
         pdf.fill_color "000000"
         pdf.text '211 112th Ave NE #312'
         pdf.text 'Bellevue, WA 98004'
@@ -23,7 +23,7 @@ module PDF
       pdf.bounding_box [2.2.in, 2.in], :width => 3.5.in, :height => 2.in do
         pdf.font "Cabin", :size => 18
         pdf.text order.user.name
-        pdf.font "Helvetica", :size => 14
+        pdf.font "Cabin", :size => 14
         pdf.fill_color "000000"
         pdf.text order.address1
         if order.address2
