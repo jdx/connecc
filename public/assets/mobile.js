5599aa5f669e8d155d4cb35c153e6759fd66b824
Jeff Dickey <dickeytk@gmail.com>
2011-02-08 23:19:40 -0800

    Added validation to card search

--- public/assets/mobile.js
+++ public/assets/mobile.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("#find-code-button").click(function(){$.mobile.changePage("/"+$("#find-code-input").val())})});
+$(document).ready(function(){$("#find-code-button").click(function(){var a=$("#find-code-input").val();if(!/^[a-zA-Z0-9]{5}$/.test(a)){alert("Code must be 5 letters/numbers")}else{$.ajax({url:"/"+a,success:function(b){$.mobile.changePage("/"+a)},error:function(b){alert("Error: No card found")},})}})});
--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
     $('#find-code-button').click(function() {
-        $.mobile.changePage("/" + $('#find-code-input').val());
+        var code = $('#find-code-input').val();
+        if (!/^[a-zA-Z0-9]{5}$/.test(code)) {
+            alert('Code must be 5 letters/numbers');
+        }
+        else {
+            $.ajax({
+              url: '/' + code,
+              success: function(data) {
+                           $.mobile.changePage("/" + code);
+                       },
+              error: function(data) {
+                        alert('Error: No card found');
+              },
+            });
+        }
     });
 });
